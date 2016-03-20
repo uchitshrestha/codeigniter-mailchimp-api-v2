89ebfdf116b379add19fc2a97c1c431eb97145a1
uchitshrestha <uchit.shrestha@gmail.com>
2016-03-20 15:33:00 +0545

    Update mailchimp_library.php

--- libraries/mailchimp_library.php
+++ libraries/mailchimp_library.php
@@ -59,15 +59,18 @@ class Mailchimp_library
 
 		$url = $this->api_endpoint.'/'.$method.'.json';
 
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
-		$result = curl_exec($ch);
-		curl_close($ch);
+	$ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
+
+        $result = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
 
 		return $result ? json_decode($result, true) : false;
 	}
